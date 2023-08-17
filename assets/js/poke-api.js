3703b82904b1c5170075ecf24eeb2867aa36b75a
Abraham <abrahamluccas13@gmail.com>
2023-08-17 16:16:41 -0300

    pequenos ajustes

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,12 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset, limit) => {
-    const offset = 0;
-    const limit = 10;
-    const url='https://pokeapi.co/api/v2/pokemon?offset=0&limit=10';
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+    const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`; 
+// aqui no lugar dos número você pega o valor, que a função recebe e joga na url
 
     return fetch(url)
         .then((response)=>response.json())
-        .then((jsonBody)=>jsonBody.results)
+        .then((jsonBody)=>jsonBody.results) // falta fazer o tratamento dos dados aqui
         .catch((error)=>console.error(error))
 }
 
