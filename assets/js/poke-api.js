0314af2d2cd9d7e2505779bdccbcd5b78fb3916f
Luciana Leal Nader <118460220+luciananader@users.noreply.github.com>
2023-12-04 09:34:57 -0300

    Fiz o README

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,15 +5,19 @@ function convertPokeApiDetailToPokemon (pokeDetail){
     pokemon.name = pokeDetail.name
     pokemon.number = pokeDetail.id
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
+    newFunction()
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
+
+    function newFunction() {
+        const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+        const [type] = types
+
+        pokemon.types = types
+        pokemon.type = type
+    }
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
